cc7ef1c51447cbbb54b5b6ee8216a6278db01acb
Riya Agrawal <65159160+RiyaAgrawal17@users.noreply.github.com>
2021-04-01 20:09:15 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
   const classes = useStyles();
 
   function resume() {
-    window.location.href="https://drive.google.com/file/d/1q2VZc82Ycj1vB3aSDK9xVQ9AR182uhJ1/view?usp=sharing"
+    window.location.href="https://drive.google.com/file/d/1wS8BYpQc0TxwnYzJZJZafbJ4XeKOTKJr/view?usp=sharing"
     }
 
   return (
